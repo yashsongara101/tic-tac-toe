7db50d1be06caab2dcbdce2f1120836d8b6a031c
yashsongara101 <yashsongara101@gmail.com>
2023-12-06 12:41:35 +0530

    Added the basic ui and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
+  const [score, setScore] = useState({
+    x: 0,
+    o: 0
+  });
+
+  // id true = X's turn, false = O's turn
+  const [turnTracker, setTurnTracker] = useState(true);
+
+  const [gameBoard, setGameBoard] = useState([
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+  ]);
+
+  useEffect(() => {
+    console.log(gameBoard);
+    // console.log(turnTracker);
+  }, [gameBoard, turnTracker]);
+
+  function playTurn (targetView) {
+    const id = targetView.id;
+    const board = gameBoard;
+
+    if(typeof board[id[0]][id[1]] === 'boolean') {
+      return;
+    }
+
+    board[id[0]][id[1]] = turnTracker;
+
+    targetView.innerHTML = turnTracker ? '❌' : '⭕';
+
+    setGameBoard(board);
+    setTurnTracker(!turnTracker);
+  }  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      border: '2px solid grey',
+      margin: 24,
+      borderRadius: 16
+    }}>
+      <ScoreBoard score={score}/>
+
+      <p>{`${turnTracker ? '❌' : '⭕'} turn`}</p>
+
+      <GameBoard playTurn={playTurn}/>
     </div>
   );
 }
